50481b0bea4025dfbe53f1c27583c6f27e167545
Zach Morse <zach.morse@connect.qut.edu.au>
2025-03-08 17:13:39 +1000

    Add boiler plate code to Address class.

--- src/main/java/org/payroll/hr/Address.java
+++ src/main/java/org/payroll/hr/Address.java
@@ -1,4 +1,21 @@
 package org.payroll.hr;
 
 public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String postCode;
+
+    public Address(String street, String city, String state, String postCode){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Street: " + this.street
+                +"\nCity: " + this.city
+                +"\nState: " + this.state
+                +"\npostCode: " + this.postCode;
+    }
+
 }
